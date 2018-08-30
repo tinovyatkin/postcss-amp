357f6d364df0c9bba53f9785d2152dffab73c0f5
Yvain Liechti <yliechti@clever-age.com>
2018-08-30 10:45:17 +0200

    fix lint

--- test-common.js
+++ test-common.js
@@ -1,13 +1,13 @@
-const postcss = require('postcss');
+const postcss = require('postcss')
 
-const plugin = require('./');
+const plugin = require('./')
 
-function run(input, output, opts = { }) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then( result => {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
+function run (input, output, opts = { }) {
+  return postcss([plugin(opts)]).process(input)
+    .then(result => {
+      expect(result.css).toEqual(output)
+      expect(result.warnings().length).toBe(0)
+    })
 }
 
-module.exports = run;
+module.exports = run
